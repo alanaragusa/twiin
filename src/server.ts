dfb836fd16a18245198d4f7fafb4fac6b8b77d9c
Alana Ragusa <alana.ragusarabello@gmail.com>
2022-04-19 22:03:49 -0300

    added routes folder

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,4 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  return response.json({ message: "hello twiin!" });
-});
-
-app.post("/colecao", (request, response) => {
-  const { name } = request.body;
-  return response.json({ name });
-});
-
 app.listen(3000, () => console.log("server is running!"));
