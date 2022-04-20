7d25364b17289dd4354fa98c7f49110f3f280b28
Alana Ragusa <alana.ragusarabello@gmail.com>
2022-04-19 21:57:40 -0300

    debug and added app.post on server.ts file

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import express from "express";
 
-const app = express();  
+const app = express();
+
+app.use(express.json());
 
 app.get("/", (request, response) => {
-    return response.json({ message: "hello twiin!"});
+  return response.json({ message: "hello twiin!" });
+});
+
+app.post("/colecao", (request, response) => {
+  const { name } = request.body;
+  return response.json({ name });
 });
 
 app.listen(3000, () => console.log("server is running!"));
