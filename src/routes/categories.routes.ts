654b4fdf194b89bdda15d24c61b44001da11c156
Alana Ragusa <alana.ragusarabello@gmail.com>
2022-04-19 22:23:18 -0300

    added categoriesRoutes and categoriesRoutes.post

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,3 +1,18 @@
 import { Router } from "express";
 
-const routes = Router();
+const categoriesRoutes = Router();
+
+const categories = [];
+
+categoriesRoutes.post("/categories", (request, response) => {
+  const { name, description } = request.body;
+
+  categories.push({
+    name,
+    description,
+  });
+
+  return response.status(201).send();
+});
+
+export { categoriesRoutes };
